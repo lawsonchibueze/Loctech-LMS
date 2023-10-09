320043d4672321c3f641b3d7f3b338c91d1c93d8
Lawson Chibueze <speak2lawson@gmail.com>
2023-10-09 17:00:52 +0100

    added Prisma database planetScale

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,7 @@
+import { Divide } from "lucide-react";
+
+const CourseIdPage = ({ params }: { params: { courseId: string } }) => {
+  return <div>Course Id: {params.courseId}</div>;
+};
+
+export default CourseIdPage;
