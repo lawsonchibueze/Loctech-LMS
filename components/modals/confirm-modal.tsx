852b8ebb2380f9ad16f67785ac6d0c109e35c782
Lawson Chibueze <speak2lawson@gmail.com>
2023-10-14 10:02:38 +0100

    added published courses

--- components/modals/confirm-modal.tsx
+++ components/modals/confirm-modal.tsx
@@ -4,7 +4,7 @@ import {
   AlertDialog,
   AlertDialogAction,
   AlertDialogCancel,
-  //   AlertDialogContent,
+  AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
@@ -21,18 +21,18 @@ export const ConfirmModal = ({ children, onConfirm }: ConfirmModalProps) => {
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
-      {/* <AlertDialogContent> */}
-      <AlertDialogHeader>
-        <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-        <AlertDialogDescription>
-          This action cannot be undone.
-        </AlertDialogDescription>
-      </AlertDialogHeader>
-      <AlertDialogFooter>
-        <AlertDialogCancel>Cancel</AlertDialogCancel>
-        <AlertDialogAction onClick={onConfirm}>Continue</AlertDialogAction>
-      </AlertDialogFooter>
-      {/* </AlertDialogContent> */}
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
     </AlertDialog>
   );
 };
