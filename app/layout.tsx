efb2b3a78bd3dbe43177792268eba0f0c13647c8
Lawson Chibueze <speak2lawson@gmail.com>
2023-10-14 11:14:09 +0100

    completed course creation confetti

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
+import { ConfettiProvider } from "@/components/providers/confetti-provider";
+import { ToasterProvider } from "@/components/providers/toaster-provider";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +21,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ConfettiProvider />
+          <ToasterProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
