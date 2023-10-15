44d302534d0b2a7c4943b4ee7768f44200641283
Lawson Chibueze <speak2lawson@gmail.com>
2023-10-15 18:19:37 +0100

    updated Logo

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,11 +1,18 @@
-import { Logo } from "./logo";
+// import { Logo } from "./logo";
+import Link from "next/link";
 import { SidebarRoutes } from "./sidebar-routes";
+// import logo from "./logo.png";
+import Image from "next/image";
 
 const Sidebar = () => {
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
       <div className="p-6">
-        <Logo />
+        <Link href="/">
+          <Image src="/logo.png" height={100} width={100} alt="logo" />
+        </Link>
+
+        {/* <Logo /> */}
       </div>
       <div className="flex flex-col w-full">
         <SidebarRoutes />
