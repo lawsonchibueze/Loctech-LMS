5660a5640ccc910a3015c41262470a975bc770a9
Lawson Chibueze <speak2lawson@gmail.com>
2023-10-17 15:26:58 +0100

    added isTeacher

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,6 +1,6 @@
 export const isTeacher = (userId?: string | null) => {
   return (
     userId === process.env.NEXT_PUBLIC_TEACHER_ID ||
-    process.env.NEXT_PUBLIC_TEACHERS_ID
+    userId === process.env.NEXT_PUBLIC_TEACHERS_ID
   );
 };
