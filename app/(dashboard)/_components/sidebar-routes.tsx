df8f705f6bc453ff8612c993c8700f59d82e88ca
Lawson Chibueze <speak2lawson@gmail.com>
2023-10-24 17:29:53 +0100

    fixed mux env

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -7,14 +7,14 @@ import { SidebarItem } from "./sidebar-item";
 
 const guestRoutes = [
   {
+    icon: Compass,
+    label: "Courses",
+    href: "/search",
+  },
+  {
     icon: Layout,
     label: "Dashboard",
     href: "/",
-  },
-  {
-    icon: Compass,
-    label: "Browse",
-    href: "/search",
   },
 ];
 
